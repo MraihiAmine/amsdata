2c4b0765f875907e4f2ff1abc78bb9af880c4255
Mraihi Amine <mraihiamin@gmail.com>
2022-02-20 20:24:51 +0100

    add databaseconfig

--- src/main/java/com/sip/amsdata/configuration/DatabaseConfig.java
+++ src/main/java/com/sip/amsdata/configuration/DatabaseConfig.java
@@ -6,13 +6,13 @@ import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
 @Configuration
 public class DatabaseConfig {
-	@Value("${spring.datasource.url}")
-	private String dbUrl;
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
 
-	@Bean
-	public DataSource dataSource() {
-		HikariConfig config = new HikariConfig();
-		config.setJdbcUrl(dbUrl);
-		return new HikariDataSource(config);
-	}
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
 }
